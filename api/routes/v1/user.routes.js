abd0f3716185a021fb92d94555979bdd76112c74
Abdulhakeem Muhammed <dev.abdulhakeem@gmail.com>
2024-01-23 14:57:01 +0100

    first commit

---
+++ api/routes/v1/user.routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  deleteUser,
+  deleteUsers,
+  getUser,
+  getUsers,
+  updateUser,
+  getUsersWithSearch,
+} from "../../controllers/user.controller.js";
+import { isAdmin } from "../../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", isAdmin, getUsers);
+router.get("/", getUsersWithSearch);
+router.get("/:userId", getUser);
+router.patch("/:userId", updateUser);
+router.delete("/", deleteUser);
+router.delete("/", isAdmin, deleteUsers);
+
+export default router;
